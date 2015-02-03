d47ffaba58f77b82b6c14e2e5a55c5fb68eb0621
Henry J Schmale <hschmale16@gmail.com>
2015-02-03 14:55:15 -0500

    Forgot to remove an sqlite depend

--- sqlCallbacks.h
+++ sqlCallbacks.h
@@ -10,7 +10,7 @@
  * All callbacks are prefixed with a `cb_`
  */
 
-#include <sqlite3.h> // might as well as this is callbacks for sqlite3
+// #include <sqlite3.h> // might as well as this is callbacks for sqlite3
 
 /** \brief an empty callback
  */
